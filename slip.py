3402596eb32bdad3f41a3f06dfa9d7701fec6ffc
MatheusMenecucci23 <menecucci40@gmail.com>
2023-07-30 15:31:27 -0300

    passo1

--- slip.py
+++ slip.py
@@ -51,7 +51,12 @@ class Enlace:
         # TODO: Preencha aqui com o código para enviar o datagrama pela linha
         # serial, fazendo corretamente a delimitação de quadros e o escape de
         # sequências especiais, de acordo com o protocolo CamadaEnlace (RFC 1055).
-        pass
+        datagrama_slip = b'\xC0' + datagrama + b'\xC0'
+
+
+        self.linha_serial.enviar(datagrama_slip)
+
+
 
     def __raw_recv(self, dados):
         # TODO: Preencha aqui com o código para receber dados da linha serial.
